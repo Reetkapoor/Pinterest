a4c88662bafedf5ff6462057cd070af15aedffdf
Reet Kapoor <reetkapoor2915@gmail.com>
2026-01-23 01:02:14 +0530

    First commit

---
+++ script.js
@@ -0,0 +1,41 @@
+const accessKey = "gj_47krc7-hHwCzrnIC9hO9Ray6bQfyZ7E2dbufLE6g";
+const searchForm = document.getElementById("search-form");
+const searchBox = document.getElementById("search-box");
+const searchResult = document.getElementById("search-result");
+const showMoreBtn = document.getElementById("show-more-btn")
+
+let keyword = "";
+let page = 1;
+
+async function searchImages() {
+  keyword = searchBox.value;
+  const url = `https://api.unsplash.com/search/photos?page=${page}&query=${keyword}&client_id=${accessKey}&per_page=12`;
+
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+
+    const results = data.results; // Correct variable name here
+    results.forEach((result) => { // Changed map to forEach for appending images
+      const image = document.createElement("img");
+      image.src = result.urls.small;
+      
+      const imagelink = document.createElement("a");
+      imagelink.href = result.links.html;
+      imagelink.target = "_blank";
+      imagelink.appendChild(image);
+
+      searchResult.appendChild(imagelink);
+    });
+    showMoreBtn.style.display="block";
+  } catch (error) {
+    console.error('Error fetching images:', error);
+  }
+}
+
+searchForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  page = 1;
+  searchResult.innerHTML = ""; // Clear previous results
+  searchImages();
+});
